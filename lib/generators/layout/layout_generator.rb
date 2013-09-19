1ce7ca3c3eaca5dc4205d283c03462511bcee149
Daniel Kehoe <daniel@danielkehoe.com>
2013-09-18 20:54:09 -0700

    update README

--- lib/generators/layout/layout_generator.rb
+++ lib/generators/layout/layout_generator.rb
@@ -1,6 +1,6 @@
 require 'rails/generators'
 
-module RailsLayout
+module Layout
   module Generators
     class LayoutGenerator < ::Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
