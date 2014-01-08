7fd44b4ec8ae9e24748cd16163df2c11f494bb5f
Daniel Kehoe <daniel@danielkehoe.com>
2014-01-07 18:20:57 -0800

    add mixins and Foundation styles for Devise views

--- lib/generators/views/views_generator.rb
+++ lib/generators/views/views_generator.rb
@@ -8,26 +8,26 @@ module Views
 
       # Add stylesheet for Devise views
       def add_devise_stylesheet
-        copy_file 'devise.css.scss', 'app/assets/stylesheets/devise.css.scss'
-      end
-
-      # Add Devise views
-      def add_devise_views
+        dir = File.expand_path("../templates", __FILE__)
         case framework_name
           when 'none'
-            remove_file 'app/assets/stylesheets/devise.css.scss'
-            remove_file 'app/views/devise/sessions/new.html.erb'
+            # TODO
           when 'simple'
             # TODO
           when 'bootstrap2'
             # TODO
           when 'bootstrap3'
-            copy_file 'sessions/new.html.erb', 'app/views/devise/sessions/new.html.erb'
+            append_file 'app/assets/stylesheets/framework_and_overrides.css.scss', File.read("#{dir}/bootstrap3.css.scss")
           when 'foundation4'
             # TODO
           when 'foundation5'
-            # TODO
+            append_file 'app/assets/stylesheets/framework_and_overrides.css.scss', File.read("#{dir}/foundation5.css.scss")
         end
+      end
+
+      # Add Devise views
+      def add_devise_views
+        copy_file 'sessions/new.html.erb', 'app/views/devise/sessions/new.html.erb'
       end
 
     end
