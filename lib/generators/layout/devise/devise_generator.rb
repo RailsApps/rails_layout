2a799b97cd2112f9d035f53a315cb0a0ae7dcd6b
Daniel Kehoe <daniel@danielkehoe.com>
2014-02-24 19:27:24 -0800

    add 'name' field to Devise registration forms if 'name' is an attribute in the User object

--- lib/generators/layout/devise/devise_generator.rb
+++ lib/generators/layout/devise/devise_generator.rb
@@ -8,7 +8,6 @@ module Layout
 
       desc "Copies Devise views to your application, with styling for a front-end framework."
 
-      # Add stylesheet for Devise views
       def add_devise_stylesheet
         dir = File.expand_path("../templates", __FILE__)
         case framework_name
@@ -27,12 +26,33 @@ module Layout
         end
       end
 
-      # Add Devise views
       def add_devise_views
         copy_file 'sessions/new.html.erb', 'app/views/devise/sessions/new.html.erb'
         copy_file 'passwords/new.html.erb', 'app/views/devise/passwords/new.html.erb'
         copy_file 'registrations/new.html.erb', 'app/views/devise/registrations/new.html.erb'
         copy_file 'registrations/edit.html.erb', 'app/views/devise/registrations/edit.html.erb'
+      end
+
+      def add_name_field
+        if Object.const_defined?('User')
+          if User.column_names.include? 'name'
+            gsub_file 'app/views/devise/registrations/new.html.erb', /:autofocus => true, /, ''
+            gsub_file 'app/views/devise/registrations/edit.html.erb', /:autofocus => true, /, ''
+            inject_into_file 'app/views/devise/registrations/new.html.erb', name_field, :before => "      <%= f.label :email %>"
+            inject_into_file 'app/views/devise/registrations/edit.html.erb', name_field, :before => "      <%= f.label :email %>"
+          end
+        end
+      end
+
+      private
+
+      def name_field
+<<-TEXT
+      <%= f.label :name %>
+      <%= f.text_field :name, :autofocus => true, class: 'form-control' %>
+    </div>
+    <div class="form-group">
+TEXT
       end
 
     end
