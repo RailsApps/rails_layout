0c7987ab1f5d56e2babb0ea789bfddfd065ef825
Daniel Kehoe <daniel@danielkehoe.com>
2016-09-05 12:33:07 +0800

    fix quotes  in navlinks for Bootstrap 4

--- lib/generators/layout/install/install_generator.rb
+++ lib/generators/layout/install/install_generator.rb
@@ -108,9 +108,9 @@ module Layout
         ext = app.config.generators.options[:rails][:template_engine] || :erb
         unless framework_name == 'none'
           # ABOUT
-          append_file 'app/views/layouts/_navigation_links.html.erb', "<li class=\"nav-item\"><%= link_to 'About', page_path('about'), class: 'nav-link' %></li>\n" if File.exists?("app/views/pages/about.html.#{ext}")
+          append_file 'app/views/layouts/_navigation_links.html.erb', "<li class='nav-item'><%= link_to 'About', page_path('about'), class: 'nav-link' %></li>\n" if File.exists?("app/views/pages/about.html.#{ext}")
           # CONTACT
-          append_file 'app/views/layouts/_navigation_links.html.erb', "<li class=\"nav-item\"><%= link_to 'Contact', new_contact_path, class: 'nav-link' %></li>
+          append_file 'app/views/layouts/_navigation_links.html.erb', "<li class='nav-item'><%= link_to 'Contact', new_contact_path, class: 'nav-link' %></li>
 \n" if File.exists?("app/views/contacts/new.html.#{ext}")
         end
       end
